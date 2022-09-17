55b947d3d696f1ed8251343b99dbaff5a4c3c50e
Nazar Ostafiichuk <ostafii4uk@gmail.com>
2022-09-17 17:47:14 +0300

    Init App

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Header />
+</template>
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  ul {
+    list-style: none;
+  }
+</style>
+
+<script>
+  import Header from './components/header/Header.vue'
+  
+  export default {
+    name: 'App',
+    components: { Header }
+  }
+</script>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,8 @@
+<template src='./Header.html' />
+<style lang='scss' rel='stylesheet/scss' scoped src='../../assets/scss/header/Header.scss' />
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
