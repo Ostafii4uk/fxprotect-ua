cebd11810a1f2f1cca26bdefd8ce03b44745a170
Nazar Ostafiichuk <ostafii4uk@gmail.com>
2022-09-19 23:39:31 +0300

    Added SelectedProjects component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <Header />
   <Preview />
   <AboutProduct />
+  <SelectedProjects />
 </template>
 
 <style lang='scss' rel='stylesheet/scss' src='./assets/scss/main.scss' />
@@ -10,9 +11,10 @@
   import Header from './components/header/Header.vue'
   import Preview from './components/preview/Preview.vue'
   import AboutProduct from './components/about-product/AboutProduct.vue'
+  import SelectedProjects from './components/selected-projects/SelectedProjects.vue'
   
   export default {
     name: 'App',
-    components: { Header, Preview, AboutProduct }
+    components: { Header, Preview, AboutProduct, SelectedProjects }
   }
 </script>
---
+++ src/components/selected-projects/SelectedProjects.vue
@@ -0,0 +1,8 @@
+<template src='./SelectedProjects.html' />
+<style lang='scss' rel='stylesheet/scss' scoped src='../../assets/scss/selected-projects/selected-projects.scss' />
+
+<script>
+export default {
+  name: 'SelectedProjects',
+}
+</script>
